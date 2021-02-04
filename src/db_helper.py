0cec28a560bdb87c6eb8216126b3192e91c18ae6
Shashanth <shashanth@datagrokr.com>
2021-02-04 12:48:01 +0530

    initial commit

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,15 +1,36 @@
+
+import mysql.connector
+
 class DbHelper:
     def get_maximum_salary(self):
         '''
         Implement the logic to find and return maximum salary from employee table
         '''
-        return None
+
+        mydb = mysql.connector.connect(host="localhost",user="shashanth",password="password",database="employees")
+    
+        mycursor = mydb.cursor()
+    
+        mycursor.execute("select * from employee_details order by salary desc ")   #passing the query
+
+        for row in mycursor.fetchone(): 
+            return row
+        
 
     def get_minimum_salary(self):
         '''
         Implement the logic to find and return minimum salary from employee table
         '''
-        return None
+
+        mydb = mysql.connector.connect(host="localhost",user="shashanth",password="password",database="employees")
+    
+        mycursor = mydb.cursor()
+    
+        mycursor.execute("select * from employee_details order by salary ")   #passing the query
+
+        for row in mycursor.fetchone(): 
+            return row
+
 
 if __name__ == "__main__":
     db_helper = DbHelper()
